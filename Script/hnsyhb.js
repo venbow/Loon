b6b03f12a23c4de67f8f4f734625e126a5b0f520
Kenneth Markland <162736075+venbow@users.noreply.github.com>
2025-07-08 19:40:44 +0800

    Update hnsyhb.js

--- Script/hnsyhb.js
+++ Script/hnsyhb.js
@@ -1,6 +1,7 @@
 const path1 = "/v1/get_purchase_list";
 const path2 = "/v1/get_vip_info_h5";
 const path3 = "/v1/get_user_info";
+const path4 = "/v1/get_enlightening_book_info";
 
 let key = {"expire_time":7955110875,"vip_type":1,"last_product_id":"com.ihuman.book.sub.vip1y"};
 
@@ -15,6 +16,11 @@ if ($request.url.indexOf(path2) != -1){
 }
 if ($request.url.indexOf(path3) != -1){
 	obj.result.userinfo["vip_status"] = key;
+}
+if ($request.url.indexOf(path4) != -1){
+	obj.result.books.forEach(function (item, idnex, array) {
+	    item.is_vip = 0;
+	})
 }
 $done({body: JSON.stringify(obj)});
 
