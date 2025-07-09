8517882ce6bd28f6c3d4d49e4286f008b732fca0
Kenneth Markland <162736075+venbow@users.noreply.github.com>
2025-07-09 17:18:24 +0800

    Update xblong.js

--- Script/xblong.js
+++ Script/xblong.js
@@ -1,11 +1,13 @@
 let body = $response.body;
 
 let modifiedBody = body
+    .replace(/"buyBigVip"\s*:\s*-?\d+/g, '"buyBigVip":1')
     .replace(/"vip"\s*:\s*-?\d+/g, '"vip":1')
     .replace(/"unpaid_vip"\s*:\s*\d+/g, '"unpaid_vip":0')
     //.replace(/"new"\s*:\s*\d+/g, '"new":0')
     .replace(/"expire"\s*:\s*-?\d+/g, '"expire":2067552000')
     .replace(/"exp_vip"\s*:\s*-?\d+/g, '"exp_vip":2067552000')
+    .replace(/"member_goods"\s*:\s*\[\]/g, '"member_goods":["30003"]');
     .replace(/"viptime"\s*:\s*\d+/g, '"viptime":315360000');
 
 $done({ body: modifiedBody });
