15ed068e69da2c89802a5f4d88c158d5ab4cbfa1
Kenneth Markland <162736075+venbow@users.noreply.github.com>
2025-08-22 11:28:51 +0800

    Update panzy.js

--- Script/panzy.js
+++ Script/panzy.js
@@ -8,14 +8,34 @@ const list = {
   //'1Blocker': { name: 'premium', id: 'blocker.ios.subscription.yearly' }
 };
 const data = {
-    "expires_date": "2099-02-18T07:52:54Z",
-    "original_purchase_date": "2025-08-20T07:52:55Z",
-    "purchase_date": "2025-08-20T07:52:54Z",
-    "ownership_type": "PURCHASED",
-    "store": "app_store"
+    "grace_period_expires_date": null,
+    "expires_date": null
+    "purchase_date": "2025-08-20T07:52:54Z"
+    //"ownership_type": "PURCHASED",
+    //"store": "app_store"
     //"is_sandbox": false
 };
-
+const data1 = {
+    "price": {
+      "amount": 0,
+      "currency": "USD"
+    },
+    "purchase_date": "2025-08-20T07:52:54Z",
+    "display_name": null
+};
+const data2 = {
+    "store_transaction_id": "500002333800066",
+    "is_sandbox": false,
+    "id": "o1_urkDyNzJcaoUmLf5U30QbA",
+    "price": {
+        "amount": 0,
+        "currency": "USD"
+    },
+    "display_name": null,
+    "original_purchase_date": "2025-08-20T07:52:54Z",
+    "purchase_date": "2025-08-20T07:52:54Z",
+    "store": "app_store"
+}
 if (typeof $response == "undefined") {
     delete $request.headers["x-revenuecat-etag"]; // prevent 304 issues
     delete $request.headers["X-RevenueCat-ETag"];
@@ -26,9 +46,12 @@ if (typeof $response == "undefined") {
     for (const i in list) {
         if (new RegExp(`^${i}`, `i`).test(ua)) {
             obj.subscriber.subscriptions = {};
-            obj.subscriber.subscriptions[list[i].id] = data;
+            obj.subscriber.other_purchases[list[i].id] = data1;
+            obj.subscriber.non_subscriptions[list[i].id] = data2;
+            //obj.subscriber.subscriptions[list[i].id] = data;
             obj.subscriber.entitlements[list[i].name] = JSON.parse(JSON.stringify(data));
             obj.subscriber.entitlements[list[i].name].product_identifier = list[i].id;
+            obj.subscriber.original_purchase_date = "2025-08-20T07:52:54Z"
             break;
         }
     }
