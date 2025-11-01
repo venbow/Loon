0dfe34f9c42ea6301f36ab7301c7a8ce19240112
Kenneth Markland <162736075+venbow@users.noreply.github.com>
2025-11-01 13:56:39 +0800

    Update Duanmedia.js

--- Script/Duanmedia.js
+++ Script/Duanmedia.js
@@ -7,12 +7,12 @@ if (url.indexOf('user/me/subscription') !== -1) {
       .replace(/message="很遗憾，您没有有效的订阅"/g, 'message="订阅有效，欢迎体验会员服务"')
       .replace(/term="false"/g, 'term="true"');
     $done({ body });
-
 } else if (url.indexOf('account/token/generate') !== -1) {
     let body = $response.body;
     body = body.replace(/allow":false/g, '"allow":true');
     $done({ body });
-
 } else {
+    let body = $response.body;
+    body = body.replace(/state="inactive"/g, 'state="active"')
     $done({});
 }
