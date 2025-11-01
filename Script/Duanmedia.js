1dd8bcfe1e07fdfd4d8bc41a6f80c45e8d372ed4
Kenneth Markland <162736075+venbow@users.noreply.github.com>
2025-11-01 13:20:04 +0800

    Add Duanmedia.js for subscription and token handling

---
+++ Script/Duanmedia.js
@@ -0,0 +1,18 @@
+const url = $request.url;
+
+if (url.indexOf('user/me/subscription') !== -1) {
+    let body = $response.body;
+    body = body
+      .replace(/state="inactive"/g, 'state="active"')
+      .replace(/message="很遗憾，您没有有效的订阅"/g, 'message="订阅有效，欢迎体验会员服务"')
+      .replace(/term="false"/g, 'term="true"');
+    $done({ body });
+
+} else if (url.indexOf('account/token/generate') !== -1) {
+    let body = $response.body;
+    body = body.replace(/allow":false/g, '"allow":true');
+    $done({ body });
+
+} else {
+    $done({});
+}
