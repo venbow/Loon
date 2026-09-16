fb138b1fd43f9c30b8720c73b59d49b14005a8a4
Kenneth Markland <162736075+venbow@users.noreply.github.com>
2026-09-16 17:58:58 +0800

    Update features

--- Script/flo.js
+++ Script/flo.js
@@ -6,7 +6,7 @@ if ($request.url.includes('/v1/apple')) {
         used_intro_gr1: true,
         auto_renew_status: false,
         expiration_date: "2999-09-09T06:42:17Z",
-        features: ["cycle_trends_widget","library","pregnancy_details","premium","premium_partnership","stories","symptom_patterns","ttc_sex_widget","virtual_assistant"],
+        features: ["report_for_doctor","symptom_checker","cycle_trends_widget","library","pregnancy_details","premium","premium_partnership","stories","symptom_patterns","ttc_sex_widget","track","virtual_assistant"],
         subscription_manager: null,
     });
     $done({
