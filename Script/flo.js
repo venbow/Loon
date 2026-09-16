7f0214045c1892931ec90ac2cc95b2c51b196f3b
Kenneth Markland <162736075+venbow@users.noreply.github.com>
2026-09-16 18:06:01 +0800

    1

--- Script/flo.js
+++ Script/flo.js
@@ -5,8 +5,8 @@ if ($request.url.includes('/v1/apple')) {
         transaction_id: 000000000652310,
         used_intro_gr1: true,
         auto_renew_status: false,
-        expiration_date: "2999-09-09T06:42:17Z",
-        features: ["report_for_doctor","symptom_checker","cycle_trends_widget","library","pregnancy_details","premium","premium_partnership","stories","symptom_patterns","ttc_sex_widget","track","virtual_assistant"],
+        expiration_date: "2099-09-09T06:42:17Z",
+        features: ["basic_webinar_pass","perimenopause","family","ttc","pregnancy","report_for_doctor","symptom_checker","cycle_trends_widget","library","pregnancy_details","premium","premium_partnership","stories","symptom_patterns","ttc_sex_widget","track","virtual_assistant"],
         subscription_manager: null,
     });
     $done({
